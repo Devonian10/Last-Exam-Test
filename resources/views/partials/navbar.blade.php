87be7cbad926024ac63f4e7f9564a113e2cc1f1c
toku404 <phantom26isn@gmail.com>
2024-02-16 20:01:59 +0800

    fix: refactor ui

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,18 +1,20 @@
 <nav>
     <div class="topnav">
-        <img src="{{  asset('gambar/299385752_397934109147635_7327667729942218094_n.jpg')}}" style="width:60px; height:60px;display:flex; padding:2px; border-radius:9999px" class="left"/>
+        <img src="{{  asset('gambar/299385752_397934109147635_7327667729942218094_n.jpg')}}" style="width:60px; height:60px;display:flex; padding:2px; border-radius:9999px" class="left" />
         <a href="{{ url('/home') }}" class="">Home</a>
+        @if(Auth::check())
         <a href="{{ url('/shop') }}">Shop</a>
         <a href="{{ url('/order') }}">Order</a>
         <a href="{{ url('/history') }}">History</a>
+        @endif
         <a href="{{ url('/about') }}">About Us</a>
         @if(!Auth::check())
         <a href="{{ url('/mainlogin') }}" class="right">login</a>
         <a href="{{ url('/registration') }}" class="right">Signup</a>
-        @else 
+        @else
         <a href="{{ route('registration.logout') }}" class="right">Logout</a>
         <a class="right">Welcome, {{ Auth::user()->username }}</a>
         @endif
-        
+
     </div>
 </nav>
